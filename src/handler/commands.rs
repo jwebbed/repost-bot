08d74a462bd91daed9dbe097ec03c5e55f735f4d
Jonathan Webb <jonathan@webbdev.ca>
2021-07-27 21:50:51 -0400

    it works:

--- src/handler/commands.rs
+++ src/handler/commands.rs
@@ -33,6 +33,38 @@ impl Handler {
             }
         }
 
-        println!("pins: {:?}", pins);
+        let mut pin_cnt: HashMap<String, usize> = HashMap::new();
+        for pin in pins {
+            let user = pin.author.name;
+            let new_cnt = if pin_cnt.contains_key(&user) {
+                pin_cnt[&user] + 1
+            } else {
+                1
+            };
+            pin_cnt.insert(user, new_cnt);
+        }
+
+        let mut tuples = Vec::new();
+        for (user, cnt) in pin_cnt.iter() {
+            tuples.push((user, cnt));
+        }
+
+        tuples.sort_by_key(|x| x.1);
+        tuples.reverse();
+        println!("{:?}", tuples);
+
+        let response = format!(
+            "the chamPIoNship\n{}",
+            tuples
+                .into_iter()
+                .map(|x| format!("{}: with {} pins", x.0, x.1))
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
+
+        match msg.channel_id.say(&ctx.http, response).await {
+            Ok(_) => (),
+            Err(why) => println!("Failed to inform of REPOST: {:?}", why),
+        }
     }
 }
