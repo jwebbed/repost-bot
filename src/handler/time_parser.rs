344e2d59d0be2c4fb20495d8ea412c0a2e18a538
Jonathan Webb <jonathan@webbdev.ca>
2021-12-26 10:03:07 -0500

    get into a good state

--- src/handler/time_parser.rs
+++ src/handler/time_parser.rs
@@ -22,9 +22,8 @@ pub fn format_duration(val: Duration) -> String {
 impl fmt::Display for FormattedDuration {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let secs = self.0.as_secs();
-        let nanos = self.0.subsec_nanos();
 
-        if secs == 0 && nanos == 0 {
+        if secs == 0 {
             f.write_str("0s")?;
             return Ok(());
         }
@@ -39,44 +38,34 @@ impl fmt::Display for FormattedDuration {
         let minutes = day_secs % 3600 / 60;
         let seconds = day_secs % 60;
 
-        let millis = nanos / 1_000_000;
-        let micros = nanos / 1000 % 1000;
-        let nanosec = nanos % 1000;
+        let values = [
+            (years, "year", true),
+            (months, "month", true),
+            (days, "d", true),
+            (hours, "h", false),
+            (minutes, "m", false),
+            (seconds, "s", false),
+        ];
 
-        let ref mut started = false;
-        item_plural(f, started, "year", years)?;
-        item_plural(f, started, "month", months)?;
-        item_plural(f, started, "day", days)?;
-        item(f, started, "h", hours as u32)?;
-        item(f, started, "m", minutes as u32)?;
-        item(f, started, "s", seconds as u32)?;
-        item(f, started, "ms", millis)?;
-        item(f, started, "us", micros)?;
-        item(f, started, "ns", nanosec)?;
+        let mut count = 0;
+        for (value, name, is_plural) in values {
+            if value > 0 {
+                if count > 0 {
+                    f.write_str(" ")?;
+                }
+                write!(f, "{}{}", value, name)?;
+                if is_plural && value > 1 {
+                    f.write_str("s")?;
+                }
+                count += 1;
+            }
+
+            // Don't include more than 3 terms
+            if count >= 3 {
+                break;
+            }
+        }
+
         Ok(())
     }
-}
-
-fn item_plural(f: &mut fmt::Formatter, started: &mut bool, name: &str, value: u64) -> fmt::Result {
-    if value > 0 {
-        if *started {
-            f.write_str(" ")?;
-        }
-        write!(f, "{}{}", value, name)?;
-        if value > 1 {
-            f.write_str("s")?;
-        }
-        *started = true;
-    }
-    Ok(())
-}
-fn item(f: &mut fmt::Formatter, started: &mut bool, name: &str, value: u32) -> fmt::Result {
-    if value > 0 {
-        if *started {
-            f.write_str(" ")?;
-        }
-        write!(f, "{}{}", value, name)?;
-        *started = true;
-    }
-    Ok(())
 }
